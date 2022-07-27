748aa4bdb1955a8171ecb095958b3358870cf3c8
Kolya132 <nikovsienko14@gmail.com>
2022-07-27 15:35:34 +0300

    add index.html file

---
+++ js hw1/hw_jsFirst.js
@@ -0,0 +1,95 @@
+const a=['str','nmt',123,'asd321',5<2,'trsSS','S','T','G','1Q','VIM']
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])
+console.log(a[9])
+console.log(a[10])
+const passwords = [
+    {name:'Anton',username:'acvwe',password:'aswefvvwdgw'},
+    {name:'Dima',username:'asderrt343',password:'1213'},
+    {name:'Sasha',username:'swqijs',password:'swijs'},
+    {name:'Kolya',username:'pieg',password: 'eter65' },
+    {name: 'Vanya',username: 'cccc',password: 'aspwjq2pqwdj'},
+    {name: 'Oleg',username: 'asaswx',password:'dididididi' },
+    {name: 'Artem',username:'qqqqqqqqqqq',password:'qzas' },
+    {name:'Roma',username:'aaaaaa',password:'qaqaq'  },
+    {name: 'Taras',username:'akakakaslwsow',password: 'lkjh' },
+    {name: 'Egor',username:'akkkkkkkk',password: 'oq' },
+    {name: 'Stas',username: 'xxsswsa',password:'mmm' },
+]
+console.log(passwords[0].password)
+console.log(passwords[1].password)
+console.log(passwords[2].password)
+console.log(passwords[3].password)
+console.log(passwords[4].password)
+console.log(passwords[5].password)
+console.log(passwords[6].password)
+console.log(passwords[7].password)
+console.log(passwords[7].password)
+console.log(passwords[8].password)
+console.log(passwords[9].password)
+console.log(passwords[10].password)
+const firstBook={
+    title:'HTML',
+    pageCount:100,
+    genre:'education',
+    authors: [
+        {name:'Stas',age:18}
+    ]
+}
+const secondBook={
+    title:'CSS',
+    pageCount:200,
+    genre: 'education',
+    authors:[
+        {name:'Artem',age:16}
+    ]
+}
+const thirdBook={
+    title:'JS',
+    pageCount:1000,
+    genre:'education',
+    authors:[
+        {name:'Pavel',age:29}
+
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
